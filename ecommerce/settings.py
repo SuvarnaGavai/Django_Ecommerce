db51c607ddb5ae4cc848f4b71782ad1040217161
SuvarnaGavai <suvarna.gavai@neosoftmail.com>
2022-11-29 13:13:56 +0530

    setting commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1&av!ce@d-o@e+&n7pn!g_7m0-m%f0k_lvud@v$nu!ffk2(&1p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+ENV_TYPE = os.environ.get('ENV_TYPE')
+if ENV_TYPE == "HEROKU":
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','ecommapp.herokuapp.com']
 
